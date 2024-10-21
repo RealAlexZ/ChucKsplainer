6e560731622da103d2d6cef88fea865982fae578
RealAlexZ <zhiyuzha@umich.edu>
2024-10-21 00:16:19 -0400

    Updated styles

--- src/html_templates.py
+++ src/html_templates.py
@@ -3,13 +3,16 @@ import base64
 css = '''
 <style>
 .chat-message {
-    padding: 1.5rem; border-radius: 0.5rem; margin-bottom: 1rem; display: flex
+    padding: 1.5rem; 
+    border-radius: 0.5rem; 
+    margin-bottom: 1rem; 
+    display: flex;
 }
 .chat-message.user {
-    background-color: #2b313e
+    background-color: #2b313e;
 }
 .chat-message.bot {
-    background-color: #475063
+    background-color: #2b313e;
 }
 .chat-message .avatar {
     width: 20%;
